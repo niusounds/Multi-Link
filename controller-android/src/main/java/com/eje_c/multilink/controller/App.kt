e6183a960bc67bd6ff80f564d5169eab729ccfe8
Yuya Matsuo <niu@niusounds.com>
2017-12-14 12:46:49 +0900

    Change to in memory database

--- controller-android/src/main/java/com/eje_c/multilink/controller/App.kt
+++ controller-android/src/main/java/com/eje_c/multilink/controller/App.kt
@@ -11,7 +11,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        db = Room.databaseBuilder(this, AppDatabase::class.java, "database").build()
+        db = Room.inMemoryDatabaseBuilder(this, AppDatabase::class.java).build()
     }
 
     companion object {
