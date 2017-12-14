f01b64813854d9cfe629228cd811ac13ec944d5b
Yuya Matsuo <niu@niusounds.com>
2017-12-14 15:22:35 +0900

    Refactored

--- controller-android/src/main/java/com/eje_c/multilink/controller/MainActivity.kt
+++ controller-android/src/main/java/com/eje_c/multilink/controller/MainActivity.kt
@@ -24,28 +24,12 @@ import kotlinx.android.synthetic.main.activity_main.*
  */
 class MainActivity : AppCompatActivity() {
 
-    /**
-     * The [android.support.v4.view.PagerAdapter] that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * [android.support.v4.app.FragmentStatePagerAdapter].
-     */
-    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
-
     private val conn = object : ServiceConnection {
         override fun onServiceConnected(componentName: ComponentName, binder: IBinder) {
 
             val udpSocket = (binder as UdpSocketService.LocalBinder).service.udpSocket
             MultiLinkUdpMessenger.initialize(udpSocket)
-
-            MultiLinkUdpMessenger.onReceivePingResponse += { deviceInfo ->
-
-                // Check illegal data and process
-                processDeviceInfo(deviceInfo)
-            }
-
+            MultiLinkUdpMessenger.onReceivePingResponse += this@MainActivity::processDeviceInfo
             MultiLinkUdpMessenger.ping()
         }
 
@@ -57,7 +41,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Process {type:0} message from remote.
+     * Process {type: Message.TYPE_PING} message from remote.
      */
     private fun processDeviceInfo(deviceInfo: DeviceInfo) {
 
@@ -97,12 +81,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
-
-        // Set up the ViewPager with the sections adapter.
-        container.adapter = mSectionsPagerAdapter
+        container.adapter = SectionsPagerAdapter(supportFragmentManager)
 
         container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
